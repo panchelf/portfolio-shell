34925bb12b0d31b8bc12a5dc3b606e2f24e52a5b
FranZsco <fran4lex@gmail.com>
2026-09-14 07:44:46 -0600

    create a click triggered expandable card

---
+++ expanded-panel/expanded.js
@@ -0,0 +1,12 @@
+const panel = document.querySelector('.expand-panel');
+const toggleBtn = document.querySelector('.expand-toggle');
+
+toggleBtn.addEventListener('click', () => {
+    if (panel.classList.contains('expanded')){
+        panel.style.maxHeight = '0px';
+    } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+
+    panel.classList.toggle('expanded');
+})
